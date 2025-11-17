c9e2d65d6727daad2e53ba5f19fcbb6346cdbda0
kkh-sdbx <kyuhanoasis@gmail.com>
2025-11-17 15:56:44 +0900

    mockServer going On!!

--- mockServer.js
+++ mockServer.js
@@ -1,7 +1,24 @@
 console.log("mock server");
-const opnt = {};
+
 // 서버에 뭐가 있어야 하는지를 구상해 봐,
 /**
+ * tick => N시간마다 갱신.
  * 
+ * botX => 숫자 맞춰서 봇을 만들어 줘야 함.
+ * 
+ * opnt => 일단 가상의 서버니까, opnt를 Kick할 경우에 대비해 bot 만들어주는 함수,
+ * 
+ * showDown => 
+ * 
+ * const player = {
+ * "id":"xxxx",
+ * "currentPoint": 00000,
+ * "actions":{"point_1":"Y","point_2":"Y","point_3":"Y","point_4":"Y","point_5":"Y" },
+ * 
+ * 
+ * }
  * 
  */
+
+const fix = document.getElementById("fix");
+
