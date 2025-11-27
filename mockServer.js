b52de853b1d8927ec080e16ad9f1ce45d77d3f10
kkh-sdbx <kyuhanoasis@gmail.com>
2025-11-27 17:52:44 +0900

    ftts

--- mockServer.js
+++ mockServer.js
@@ -1,6 +1,39 @@
 console.log("mock server");
+console.log(storage);
+
+const ALLUSERS = {};
 
 
+class User{
+    constructor(userNum){
+        this.name = `user${userNum}`;
+        this.kickTickets = 3;
+        this.points = new Map([["point1",null],["point2",null],["point3",null],["point4",null],["point5",null]]);
+        this.actions = new Map([["point1","Y"],["point2","Y"],["point3","Y"],["point4","Y"],["point5","Y"]]);
+    }
+    YES(pointNum){
+        this.actions.set(`point${pointNum}`,"Y");        
+    }
+    NO(pointNum){
+        this.actions.set(`point${pointNum}`,"N");
+    }
+    KICK(pointNum){
+        this.actions.set(`point${pointNum}`,"K");
+    }
+    setID(userId){
+        this.id = userId;
+    }
+
+}
+
+const dummy = document.getElementById("dummy");
+
+dummy.addEventListener("actionFixed",(e)=>{
+    console.log(e);
+})
+function getUserInfo(){
+
+}
 
 // # Proceed, Discard 시의 행동과
 // ## 
@@ -68,23 +101,6 @@ const KBtn = document.getElementById('KBtn');
  * 매치메이킹 알고리즘이 핵심일텐데...
  * 
 ## 내가 짜둔 코드##
-class User{
-    constructor(userNum){
-        this.name = `user${userNum}`;
-        this.kickTickets = 3;
-        this.points = new Map([["point1",null],["point2",null],["point3",null],["point4",null],["point5",null]]);
-        this.actions = new Map([["point1","Y"],["point2","Y"],["point3","Y"],["point4","Y"],["point5","Y"]]);
-    }
-    YES(pointNum){
-        this.actions.set(`point${pointNum}`,"Y");        
-    }
-    NO(pointNum){
-        this.actions.set(`point${pointNum}`,"N");
-    }
-    KICK(pointNum){
-        this.actions.set(`point${pointNum}`,"K");
-    }
-}
 
 function fillUserPool(N){
     const pool = [];
