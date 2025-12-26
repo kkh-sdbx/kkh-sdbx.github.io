d3801e7ffc42bc935ca9b46fa1c41c0c0f499b80
kkh-sdbx <kyuhanoasis@gmail.com>
2025-12-26 10:30:22 +0900

    ㄴㅇㄹ

--- mockServer.js
+++ mockServer.js
@@ -352,7 +352,7 @@ setDummyActions.addEventListener("click",()=>{
 
 
 // IV. 플레이어와 Dummy 들의 action이 모두 설정된 상황, showDown() 진행
-finishRound.addEventListener("click",()={
+finishRound.addEventListener("click",()=>{
     console.log("currnet round finished - showDown starts!");
     NYK_showDown(ALLUSERS);
 });
