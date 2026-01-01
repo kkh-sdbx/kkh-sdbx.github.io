54c7b244b5c0125c90f8e653730d9ed05dc1187c
kkh-sdbx <kyuhanoasis@gmail.com>
2026-01-01 18:55:23 +0900

    frs

--- mockServer.js
+++ mockServer.js
@@ -345,7 +345,7 @@ function matchBots(listOfBots){
             // console.log(i+j);
 
             let nextLastPoint = null;
-            i+j>5 ? nextLastPoint = null: nextLastPoint = leftBots[i+j].emptySlots[leftBots[i+j].emptySlots.length-1]; //"point5";
+            leftBots.length>i+j ?  nextLastPoint = leftBots[i+j].emptySlots[leftBots[i+j].emptySlots.length-1]:nextLastPoint = null; //"point5";
             
             if(nextLastPoint){
                 leftBots[i].ref.set(leftBots[i].emptySlots[0], leftBots[i+j].name);
