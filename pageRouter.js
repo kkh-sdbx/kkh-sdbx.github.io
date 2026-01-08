b02671a87230961cfd27e0c4adc1ea7337f87400
kkh-sdbx <kyuhanoasis@gmail.com>
2026-01-08 11:24:30 +0900

    fesd

--- pageRouter.js
+++ pageRouter.js
@@ -1,8 +1,5 @@
 let PAGES = null;
-const ALLPAGES = document.querySelectorAll(".page"); 
-let current ;
-
-
+let current = null;
 
 
 const PAGEROUTER = {
@@ -10,8 +7,15 @@ const PAGEROUTER = {
         return current
     },
     moveToPage(pageName){
+        // "ALL" 예약어 호출 방지
+        if (pageName === "ALL") {
+            console.error("Cannot move to 'ALL' context.");
+            return;
+        }
+
         if(!PAGES){
             PAGES = {
+                "ALLPAGES" :document.querySelectorAll(".page"),
                 "LOADING": document.getElementById("loadingPage"),    
                 "MAIN": document.getElementById("mainPage"),
                 "GLOBAL": document.getElementById("globalMode"),
@@ -20,13 +24,13 @@ const PAGEROUTER = {
 
         }
 
-        ALLPAGES.forEach((page)=>{
+        PAGES.ALLPAGES.forEach((page)=>{
             page.classList.remove("current");
         });
 
         if(PAGES[pageName]){
-            PAGES[target].classList.add("current")
-                current = target;
+            PAGES[pageName].classList.add("current");
+            current = pageName;
 
         }else{
             console.warn(`wrong page name: ${pageName}`);
