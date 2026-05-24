506fe651403d74d743d4ece3d871d3f75c668dbd
kkh-sdbx <kyuhanoasis@gmail.com>
2026-05-24 23:33:04 +0900

    ppp

--- Connection/globalConnection.js
+++ Connection/globalConnection.js
@@ -3,6 +3,8 @@
 import SCHEMA from "../Tools/DATA_SCHEMA.js";
 
 const connectGlobalMode = ()=>{
+    let ACTION_VALIDATOR = null;
+    let POINT_VALIDATOR = null;
 
     let storage = null;
     let userDecisions = null;
@@ -17,13 +19,16 @@ const connectGlobalMode = ()=>{
     let SELECTION_EVENT_TARGET = null;
     let MODAL_EVENT_TARGET = null;
     let MOCK_SERVER_EVENT_TARGET = null;
-    let POINT_VALIDATOR = null;
+
 
     // 사실 이 정보도 서버에서 받아와야 하는거지? 추후에 수정 필요하다.
-    const setPrionerStorage = ()=>{
+    const setPrisonerStorage = ()=>{
         const DATA_SCHEMA = SCHEMA();
         PRISONER_DATA = DATA_SCHEMA.prisonerData();
         storage.clear();
+        
+        storage.setItem("id",PRISONER_DATA.PRISONER_ID);
+    
         storage.setItem("name",PRISONER_DATA.PRISONER_NAME);
     
         storage.setItem("type",PRISONER_DATA.PRISONER_TYPE);
@@ -42,14 +47,13 @@ const connectGlobalMode = ()=>{
      */
     const getPointData = (pointId)=>{
         let pointData = {"target":null, "action":null};
-        const validator = ["Y","N","K"];
         
         if(!(POINT_VALIDATOR.includes(pointId))){
             console.log("pointId invalid:", pointId);
             return
         }
         
-        if(validator.includes(storage.getItem(pointId))){
+        if(ACTION_VALIDATOR.includes(storage.getItem(pointId))){
             pointData = { "target":pointId, "action":storage.getItem(pointId)};
             
         }else{
@@ -75,7 +79,7 @@ const connectGlobalMode = ()=>{
     const handleFix = ()=>{ 
 
         userDecisions = {
-            "userId":storage.id,
+            "userId":storage.getItem("id"),
             "G_point_1":undefined,
             "G_point_2":undefined,
             "G_point_3":undefined,
@@ -83,10 +87,10 @@ const connectGlobalMode = ()=>{
             "G_point_5":undefined,
             "emptyPoints":[]
         }
-        
+
         for(const point of POINT_VALIDATOR){
             let userAction = storage.getItem(point);
-            if(userAction){ //fix 시점에서, action이 localStorage에 저장되어 있으면 result에 입력
+            if(ACTION_VALIDATOR.includes(userAction)){ //fix 시점에서, action이 localStorage에 저장되어 있으면 result에 입력
                 userDecisions[point] = userAction;
 
             }else{ //fix 시점에서, action이 localStorage에 없으면
@@ -94,8 +98,15 @@ const connectGlobalMode = ()=>{
             }
         };
         
+        
+        console.log(userDecisions);
+        //  
+        MODAL_EVENT_TARGET.dispatchEvent(new CustomEvent("checkEmptyPoints",{
+            bubbles: false,
+            cancelable: false,
+            detail:userDecisions
 
-        MODAL_EVENT_TARGET.dispatchEvent(CHECK_EMPTY_POINTS_EVENT);
+        }));
 
         
 
@@ -130,14 +141,6 @@ const connectGlobalMode = ()=>{
 
     const init = (eventTarget)=>{
         
-        //  
-        CHECK_EMPTY_POINTS_EVENT = new CustomEvent("checkEmptyPoints",{
-            bubbles: false,
-            cancelable: false,
-            detail:userDecisions
-
-        });
-
         // 지금은 localStorage를 쓰지만 모바일이든 PC버전이든 스토리지를 수정해야 한다.
         storage = window.localStorage;
 
@@ -146,22 +149,8 @@ const connectGlobalMode = ()=>{
         SELECTION_EVENT_TARGET = eventTarget.selectionEventTarget;
         MODAL_EVENT_TARGET = eventTarget.modalEventTarget;
         MOCK_SERVER_EVENT_TARGET = eventTarget.mockServerEventTarget;
+        ACTION_VALIDATOR = ["Y","N","K"];
         POINT_VALIDATOR = ["G_point_1","G_point_2","G_point_3","G_point_4","G_point_5"];
-
-        // Model은 로컬 저장소와 웹 서버 사이를 중개.
-        SEND_INFO_TO_SERVER_EVENT = new CustomEvent("sendInfoToServer",{
-            bubbles: true, // Allows the event to bubble up the DOM => 왜 이렇게 되어 있었는지 파악해야 함.
-            cancelable: false,
-            detail:ultimatum
-            }
-        );
-
-        // storage 에서 선택지 불러오기
-        G_updateActionsFromStorage = new CustomEvent("storageUpdated",{ //이 변수명인데, 왜 커스텀 이벤트로 되어있지? 
-            bubbles:true,
-            cancelable: false,
-            detail:storage
-        });
 
         // userDecision와 ultimatum이 분리된 이유=> 저장됭 값은 nully할 수 있음. 빈 칸은 Y로 채워 서버로 보낸다.
         userDecisions = {
@@ -184,15 +173,31 @@ const connectGlobalMode = ()=>{
             "timeArrived":null,
             "emptyPoints":[]
         };
+
         
-        setPrionerStorage();
+        // Model은 로컬 저장소와 웹 서버 사이를 중개.
+        SEND_INFO_TO_SERVER_EVENT = new CustomEvent("sendInfoToServer",{
+            bubbles: true, // Allows the event to bubble up the DOM => 왜 이렇게 되어 있었는지 파악해야 함.
+            cancelable: false,
+            detail:ultimatum
+            }
+        );
+
+        // storage 에서 선택지 불러오기
+        G_updateActionsFromStorage = new CustomEvent("storageUpdated",{ //이 변수명인데, 왜 커스텀 이벤트로 되어있지? 
+            bubbles:true,
+            cancelable: false,
+            detail:storage
+        });
+        
+        setPrisonerStorage();
 
     };
 
     return{
         init,
         handleFix,
-        setPrionerStorage,
+        setPrisonerStorage,
         sendUltimatumToServer,
         getPointData,
         setPointDecision
