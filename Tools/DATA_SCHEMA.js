bcdd48f1424545cfb56fcac355061cf8961897f3
kkh-sdbx <kyuhanoasis@gmail.com>
2026-05-27 23:13:37 +0900

    DATA_SCHEMA에 game phase를 const로 만들어 뒀다

--- Tools/DATA_SCHEMA.js
+++ Tools/DATA_SCHEMA.js
@@ -25,10 +25,50 @@ const SCHEMA = ()=>{
         }
         return prisoner
     
-    }
+    };
+
+    const GAME_PHASE = ()=>{
+        /**resultTable은 서버에서 받아와 저장을 하든지 해야 한다. Connection에서 setSchema()도 있어야 할 듯. */
+        const resultTable = {
+        "YY": {"breakUp":false, "score":[4, 4]},   
+        "YN": {"breakUp":false, "score":[-8, 8]},  
+        "YK": {"breakUp":true, "score":[2, 0]},
+        "NY": {"breakUp":false, "score":[8, -8]},  
+        "NN": {"breakUp":false, "score":[-6, -6]}, //NN의 시차도 적용해야 함.
+        "NK": {"breakUp":true, "score":[-2, 0]},
+        "KY": {"breakUp":true, "score":[0, 2]},   
+        "KN": {"breakUp":true, "score":[0, -2]},   
+        "KK": {"breakUp":false, "score":[-12, -12]}
+    }; 
+        const playingPhase = new Map([
+            ["commence","commencingFunctions"],
+            ["selection","selectionFunctions"],
+            ["sendingUltimatum","sendingUltimatumFunctions"],
+            ["selection","selectionFunctions"]        
+        ]);
+        
+        const resultPhase = new Map([
+            ["showDown","showDownFunctions"],
+            ["matchMaking","matchMakingFunctions"],
+            ["ending","endingFunctions"]        
+        ]);
+        const game = {
+            "rounds":0, //로컬은 13, 글로벌은 41
+            "startingLife":0, // 로컬은 100, 글로벌은 ??
+            "playingPhase": playingPhase,
+            "resultPhase":resultPhase,
+            "resultTable":resultTable
+        }; 
+        //new Map([]);
+
+        return{
+            game
+        }
+    };
     
     return{
-        prisonerData
+        prisonerData,
+        GAME_PHASE
 
     }
 
