ca25cd96745b8dc61ed8802c7b42d7a0fae4f315
kkh-sdbx <kyuhanoasis@gmail.com>
2026-09-23 13:58:55 +0900

    260923

--- Connection/mockDB.js
+++ Connection/mockDB.js
@@ -26,23 +26,26 @@ const MOCK_DB_HANDLER = ()=>{
         }else{ // 첫 로그인(DB에 정보 없음)
             alert("new user!");
 
-            //DB에 새 user를 만들고, 새 유저의 상점과 마이페이지 정보를 전달해준다. 스키마를 정할 때가 됐다.
-            // ## entryPoint: mockDB가 만들어졌다. 
-            /*
-            const userInfo = 
+            const newUserInfo = 
             {
             "renderInfo":{
                 "shop":{},
                 "mainPage":{}
                 }
             };
+
+            //DB에 새 user를 만들고, 새 유저의 상점과 마이페이지 정보를 전달해준다. 스키마를 정할 때가 됐다.
+            // ## entryPoint: mockDB가 만들어졌다. 
+            /*
+            
             이거 선언해서 리턴하고 console.log() 해 보기.
 
             DB에 입력(parse등을 포함)=> DB에서 읽어와서 유저에게 렌더링값 리턴.
 
             //## 이런 판단 로직을 DB가 갖고 있는 게 맞나? 판단 로직은 mockLoadingConnection에서 짜야 하는 것 아닌가?
             */
-            return 
+           console.log("newUserInfo: ", newUserInfo);
+            return newUserInfo
         };
         
     }; 
